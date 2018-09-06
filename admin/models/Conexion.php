ba1c60a3de979b792a72342824b18198cce1e851
Miguel Peña <miguel_alvarenga1994@hotmail.com>
2018-09-06 12:26:19 -0600

    Archivos de Conexion
    Parametros Modificados Por Defecto

--- admin/models/Conexion.php
+++ admin/models/Conexion.php
@@ -5,7 +5,7 @@
     public static function conexion()
     {
       try {
-        $conexion=new PDO('mysql:host=localhost; dbname=id5392174_penagroup_erpdev', 'id5392174_mpena', 'P@100514');
+        $conexion=new PDO('mysql:host=localhost; dbname=id5392174_penagroup_erpdev', 'root', '');
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conexion->exec("SET CHARACTER SET UTF8");
       } catch (Exception $e) {
--- db/config.php
+++ db/config.php
@@ -1,7 +1,7 @@
 <?php
   define('host', 'localhost'); //Aquí definimos el host o servidor de nuestra base de datos MySQL.
-	define('user', 'id5392174_mpena'); //Nombre del usuario propietario de la base de datos.
-	define('pass', 'P@100514'); //Contraseña de conexion a la base de datos.
+	define('user', 'root'); //Nombre del usuario propietario de la base de datos.
+	define('pass', ''); //Contraseña de conexion a la base de datos.
 	define('dbname', 'id5392174_penagroup_erpdev'); //Nombre de la base de datos
    define('charset', 'utf8');
 	//define('port', 3309); //Puerto donde escucha el servidor de MySQL
